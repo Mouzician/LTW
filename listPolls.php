90e66ba29e7fea51710a449d9caa4af7bb023ced
AndreRegado <andreregado4@gmail.com>
2014-12-01 23:40:43 +0000

    Lista o titulo/questao das polls todas

---
+++ listPolls.php
@@ -0,0 +1,38 @@
+<?php
+    include('/searchDatabase.php');
+    $dbh = new PDO('sqlite:users.db');
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>List all Polls</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="listPollscss.css">        
+    </head>
+    
+   <body>
+
+         <ul id="navigation">
+             <li class="left"><a href="menuinicial.html">Back</a></li>
+        </ul>
+        <br></br>
+            
+        <h1><u>List of all Polls</u></h1>
+
+        <?php 
+        $result = getPergunta($dbh);?>
+
+        <ul class="rolldown-list" id="myList">
+    
+       <?php foreach ($result as $temp) { ?>
+            
+            <li><a href=""><?php echo $temp['question'] ?></a></li>      
+
+     <?php } ?>
+
+   </ul>
+                
+    </body>
+</html>
+         
